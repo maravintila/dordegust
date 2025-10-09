785caba1024fd29ca572bc3ae9195b1b2b4bca13
Mara Vintila <mara.vintila@REE-PC-046.local>
2025-10-09 13:12:04 +0300

    Adaugă logica de upload imagine și corectări pentru admin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
 import sqlite3
 from functools import wraps
 import os
